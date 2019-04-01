8e45629c8bd9d86a0658da9b4f2fee83e9fd69e2
Amir Zaidi <azaidi@live.nl>
2019-04-01 16:24:14 +0200

    Launcher3: Extend notification listener with proxy
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ib5392584ad6abc4b84ab38e28f1fafb4a4ca1418

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -341,6 +341,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
 
         setupViews();
         mPopupDataProvider = new PopupDataProvider(this);
+        LauncherNotifications.getInstance().addListener(mPopupDataProvider);
 
         mAppTransitionManager = LauncherAppTransitionManager.newInstance(this);
 
@@ -930,7 +931,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
             mModel.refreshShortcutsIfRequired();
 
             // Set the notification listener and fetch updated notifications when we resume
-            NotificationListener.setNotificationsChangedListener(mPopupDataProvider);
+            NotificationListener.setNotificationsChangedListener(LauncherNotifications.getInstance());
 
             DiscoveryBounce.showForHomeIfNeeded(this);
 
