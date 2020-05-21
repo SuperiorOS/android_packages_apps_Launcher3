e2f0b0caf555bb1ea0613391155f6afb3690cc3b
hundeva <hundeva@gmail.com>
2020-05-21 13:18:56 +0200

    Launcher3: Add option to change icon size
    bootleggers edit: Adapt to Launcher3
    
    *DU Edits*
    - Fix icons not applying
    
    *404 Edit*
    - Forward port to 10
    
    Change-Id: If14470f649dd75e91e273e873081c5640786f533
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -129,6 +129,7 @@ public final class Utilities {
     public static final String SHOW_WORKSPACE_GRADIENT = "pref_show_workspace_grad";
     public static final String SHOW_HOTSEAT_GRADIENT = "pref_show_hotseat_grad";
     public static final String SLEEP_GESTURE = "pref_sleep_gesture";
+    public static final String ICON_SIZE = "pref_icon_size";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -679,6 +680,32 @@ public final class Utilities {
 
     public static boolean useSleepGesture(Context context) {
         return getPrefs(context).getBoolean(SLEEP_GESTURE, true);
+    }
+
+    public static float getIconSizeModifier(Context context) {
+        String saved = getPrefs(context).getString(ICON_SIZE, "average");
+        float offset;
+        switch (saved) {
+            case "extrasmall":
+                offset = 0.75F;
+                break;
+            case "small":
+                offset = 0.90F;
+                break;
+            case "average":
+                offset = 1.00F;
+                break;
+            case "large":
+                offset = 1.10F;
+                break;
+            case "extralarge":
+                offset = 1.25F;
+                break;
+            default:
+                offset = 1.00F;
+                break;
+        }
+        return offset;
     }
 
     public static void restart(final Context context) {
