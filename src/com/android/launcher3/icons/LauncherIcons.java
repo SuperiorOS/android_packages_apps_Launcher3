b24c4e75fb2a81e0967b019749dfcf346b0e78f4
Anay Wadhera <awadhera@berkeley.edu>
2021-03-07 00:28:56 -0800

    Launcher3: Use a custom icon factory to decide icon wrapping
    Change-Id: Ie287c918106e786cf6a2d77ca7188a2aa7556fa2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/icons/LauncherIcons.java
+++ src/com/android/launcher3/icons/LauncherIcons.java
@@ -25,12 +25,13 @@ import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
 import com.android.launcher3.util.Themes;
+import com.android.launcher3.superior.icon.SuperiorIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends SuperiorIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;
