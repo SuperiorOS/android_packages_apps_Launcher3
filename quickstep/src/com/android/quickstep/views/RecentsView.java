b3cc219419c930d2db5ecdd2e2832dcdf93e00d0
LibXZR <i@xzr.moe>
2022-04-10 17:29:14 +0800

    Launcher3: Recalculate scroll scale on layout
    Edge case exist that when there's only one task,
    onScrollChanged() won't be called so the last scale
    from the recycled view is showed, leading to wrong
    display size.
    
    Change-Id: If8a189ba6b854ed486283d43b8a0ac3e4bb4e9f2
    Signed-off-by: LibXZR <i@xzr.moe>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3663,6 +3663,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                         .setScroll(getScrollOffset()));
         setImportantForAccessibility(isModal() ? IMPORTANT_FOR_ACCESSIBILITY_NO
                 : IMPORTANT_FOR_ACCESSIBILITY_AUTO);
+        doScrollScale();
     }
 
     private void updatePageOffsets() {
