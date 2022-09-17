b045b628fc6361d8026315f23a67c8fa3cb021b4
Pranav Vashi <neobuddy89@gmail.com>
2022-09-17 17:29:13 +0530

    Launcher3: Bring back QuickSpace events
    * This is wholesome squash of work done in 11.0 of crDroid Home
    * History commits - https://github.com/crdroidandroid/android_packages_apps_Launcher3/commits/11.0/src/com/android/launcher3/quickspace
    
    Co-authored-by: Chris <chris@cypheros.co>
    Co-authored-by: El Dainosor <eldainosor@gmail.com>
    Co-authored-by: Ali B <abittin@gmail.com>
    Co-authored-by: Till Kottmann <me@deletescape.ch>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I34cdbdfd873e2098e9bb991171e0323321b88cc0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -214,6 +214,7 @@ import com.android.launcher3.pm.PinRequestHelper;
 import com.android.launcher3.popup.ArrowPopup;
 import com.android.launcher3.popup.PopupDataProvider;
 import com.android.launcher3.popup.SystemShortcut;
+import com.android.launcher3.quickspace.QuickSpaceView;
 import com.android.launcher3.statemanager.StateManager;
 import com.android.launcher3.statemanager.StateManager.StateHandler;
 import com.android.launcher3.statemanager.StatefulActivity;
@@ -414,6 +415,9 @@ public class Launcher extends StatefulActivity<LauncherState>
             enabled -> mIsNaturalScrollingEnabled = enabled;
 
     private ActivityContext mLauncher;
+
+    // QuickSpace
+    private QuickSpaceView mQuickSpace;
 
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
@@ -1060,6 +1064,9 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityStopped();
         }
+        if (mQuickSpace != null) {
+            mQuickSpace.onPause();
+        }
         hideKeyboard();
         logStopAndResume(false /* isResume */);
         mAppWidgetHolder.setActivityStarted(false);
@@ -1267,6 +1274,10 @@ public class Launcher extends StatefulActivity<LauncherState>
         TraceHelper.INSTANCE.beginSection(ON_RESUME_EVT);
         super.onResume();
 
+        if (mQuickSpace != null) {
+            mQuickSpace.onResume();
+        }
+
         if (mDeferOverlayCallbacks) {
             scheduleDeferredCheck();
         } else {
@@ -1291,6 +1302,9 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         if (!mDeferOverlayCallbacks) {
             mOverlayManager.onActivityPaused();
+        }
+        if (mQuickSpace != null) {
+            mQuickSpace.onPause();
         }
         mAppWidgetHolder.setActivityResumed(false);
     }
@@ -1374,6 +1388,9 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         // Setup Scrim
         mScrimView = findViewById(R.id.scrim_view);
+
+        // QuickSpace
+        mQuickSpace = findViewById(R.id.reserved_container_workspace);
 
         // Setup the drag controller (drop targets have to be added in reverse order in priority)
         mDropTargetBar.setup(mDragController);
@@ -1768,6 +1785,10 @@ public class Launcher extends StatefulActivity<LauncherState>
         // changes while launcher is still loading.
         getRootView().getViewTreeObserver().removeOnPreDrawListener(mOnInitialBindListener);
         mOverlayManager.onActivityDestroyed();
+        
+        if (mQuickSpace != null) {
+            mQuickSpace.onPause();
+        }
     }
 
     public LauncherAccessibilityDelegate getAccessibilityDelegate() {
